35c55d3750e5985ff53c6bb2ad2f1b4c03501019
Dreamspark <73857862+Arc-Dream@users.noreply.github.com>
2023-12-23 15:54:58 +0300

    Add files via upload

---
+++ STATIC/JS/script.js
@@ -0,0 +1,14 @@
+console.log("Script active");
+
+    
+setTimeout(() => {  
+    
+    // script=document.createElement('link');
+    // script.rel='stylesheet';
+    // script.href='/router_php/STATIC/CSS/style'
+
+    // document.getElementsByTagName('head')[0].appendChild(script);     
+
+    // location.reload();
+
+}, 500);
